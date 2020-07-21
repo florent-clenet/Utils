8c2521b84b470dba0c33759eac87db0cee91b07e
florent-clenet <florent.clenet@gmail.com>
2020-07-21 18:25:55 +0200

    list2csv fix : function do not modify input data anymore, add column delimiter as input parameter, all functions are now declared as static, adapt list2csv_test

--- python/Utils.py
+++ python/Utils.py
@@ -1,37 +1,49 @@
 import csv
 import os
+import copy
 
 class Utils(object):
 
+    @staticmethod
     def __sortKeysList(data):
-        #get all keys from all dict
+        """
+            Return listed keys from dict list
+        """
         allKeys = list()
         for x in range(0, len(data)):
-            allKeys += list(data[x].keys()) #all keys
-
+            allKeys += copy.deepcopy(list(data[x].keys())) #copy all keys
         #clean dupicated keys
-        cleanKeys = list()
-        indent_key = 0
-        for x in range (0,len(allKeys)):
-            cleanKeys.append(allKeys[0])
-            try:
-                while True:
-                    allKeys.remove(cleanKeys[indent_key])
-            except ValueError:
-                pass #do nothing
-            indent_key += 1
-            if len(allKeys) == 0:
-                break #job done
+        cleanKeys = list(dict.fromkeys(allKeys))
         return cleanKeys
         #All keys are listed, no clone anymore
 
+    @staticmethod
+    def __dict_to_list(rowdict, fieldnames, ColumnWidth):
+        """
+            From dict, return sorted values by keys
+            If key don't exist in the key, just add some spaces
+            This function do not modify input data
+        """
+        returnedList = list()
+        #for each key...
+        for key in fieldnames:
+            restval = None
+            #...if dict don't know the key, add only space " "
+            if rowdict.get(key,restval) == None:
+                returnedList.append(ColumnWidth[key] * " ")
+            #...else, if key is know by dict, add value with good amount of " " for readable csv file
+            else:
+                returnedList.append(str(rowdict[key]) + ((ColumnWidth[key] - len(str(rowdict[key]))) * " ") )
+        return returnedList
 
-    #@staticmethod
-    def list2csv(self,data=list(),fileName="report.csv",path="."):
+
+    @staticmethod
+    def list2csv(data=list(),fileName="report.csv",path='.', valueDelimiter=','):
         """
             From list of dictionaries, write all the data in a csv file.
             Data are modified before writting so you can read it easily with a simple text editor.
             All columns alignment are made by adding spaces after values/keys.
+            This function do not corrupt your input data
 
             :param data: list of dictionaries
             :param fileName: output csv file name
@@ -48,11 +60,10 @@ class Utils(object):
             |value3  |              |value3          |VeryLongValue3|
 
             It work even if dictionnaries don't have the same keys.
-            Unknow key will be added and value filles with ' '.
         """
 
         #test path and data lenght before starting
-        if (len(data) != 0) and (path != None):
+        if(len(data) != 0):
             #if file already exist, delete and create
             if os.path.isfile(path+"/"+fileName) == True:
                 os.remove(path+"/"+fileName)
@@ -66,18 +77,12 @@ class Utils(object):
                 ColumnWidth = dict()
                 for x in range (0, len(keys)):
                     ColumnWidth[keys[x]] = len(keys[x])
-                
-                #If Key is missing in dict, add it with empty string value
-                for x in range (0, len(data)):
-                    for y in range (0, len(keys)):
-                        if keys[y] not in data[x].keys():
-                            data[x][keys[y]] = ""
-
 
                 #adapt length with length of values (in case of value length > key length)
                 for i in range (0, len(data)):
                     for x in range (0, len(data[i])):
-                        width = len(str(data[i][keys[x]]))
+                        #width = len(str(data[i][keys[x]]))
+                        width = len(str(data[i].get(keys[x])))
                         if (ColumnWidth[keys[x]]) < width:
                             ColumnWidth[keys[x]] = width
                 #all column width are now listed
@@ -89,17 +94,12 @@ class Utils(object):
                     #for each column name, change width
                     keysShift[i] = keysShift[i] + ((ColumnWidth[keys[i]] - len(str(keysShift[i]))) * " ")
                 
-                #for each dictionnary, change values by adding space for column width
-                for i in range (0, len(data)):
-                    for x in range (0, len(data[i])):
-                        data[i][keys[x]] = str(data[i][keys[x]]) + ((ColumnWidth[keys[x]] - len(str(data[i][keys[x]]))) * " ")
-
                 #use csv.writer to write keys with space shift
-                writer = csv.writer(csvfile,delimiter='|', quoting=csv.QUOTE_MINIMAL)
+                writer = csv.writer(csvfile,delimiter=str(valueDelimiter), quoting=csv.QUOTE_MINIMAL)
                 writer.writerow(keysShift)
 
-                #use csv.DictWriter to write all values in the good column (fieldname option)
-                Dictwriter = csv.DictWriter(csvfile, fieldnames=keys, delimiter='|', quoting=csv.QUOTE_MINIMAL)
+                #writerow all value for each key by calling __dict_to_list
                 for i in range (0, len(data)):
-                    Dictwriter.writerow(data[i])
+                    writer.writerow(Utils.__dict_to_list(copy.deepcopy(data[i]), keys, ColumnWidth))
+        else: print("ERROR : no input data")
 
