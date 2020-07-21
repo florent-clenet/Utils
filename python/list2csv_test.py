a4d444547c487ce9261d64291bde8c15ea028362
florent.clenet <florent.clenet@gmail.com>
2020-07-21 10:59:56 +0200

    Add file Utils.py : class wich contains one function for now 'list2csv', Add list2csv_test to test this function, add __init__ python file

---
+++ python/list2csv_test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import os
+import sys
+from Utils import Utils
+sys.path.insert(0, os.path.dirname(__file__))
+
+'''
+In this exemple, we are created 4 dictionnaries with different informations (Keys/values)
+Those dictionnaries are pushed in a list called 'ListTest'
+which is passed as parameter in function list2csv from Utils.py with file name and out path
+'''
+
+Dico1=dict()
+Dico1['KEY_INFORMATION'] = 'Dict 1'
+Dico1['Some_information'] = 'me'
+Dico1['ActivationDate'] = '16/07/2020'
+Dico1['OneMoreInformation'] = 'Lalala'
+
+Dico2=dict()
+Dico2['KEY_INFORMATION'] = 'Dict 2'
+Dico2['KEYONE'] = 2
+Dico2['Signature'] = 'Mister_Bean'
+Dico2['ActivationDate'] = '17/07/2020 to 31/02/2021'
+
+Dico3=dict()
+Dico3['KEY_INFORMATION'] = 'Dict 3'
+Dico3['OneMoreInformation'] = 'Batmobile'
+Dico3['TwoMoreInformation'] = 'batarang'
+Dico3['KEYONE'] = 3
+Dico3['Signature'] = 'IMBATMAN?'
+Dico3['ActivationDate'] = 124744512558812
+
+Dico4=dict()
+Dico4['KEY_INFORMATION'] = 'Dict 4'
+Dico4['OneMoreInformation'] = 'Some_blabla_with_blibli'
+Dico4['TwoMoreInformation'] = 'This is info 2'
+Dico4['KEYONE'] = '4'
+Dico4['Signature'] = 'ThisIsMyPersonalSignature'
+Dico4['ActivationDate'] = '18/07/2020'
+
+
+ListTest = list()
+ListTest.append(Dico1)
+ListTest.append(Dico2)
+ListTest.append(Dico3)
+ListTest.append(Dico4)
+myUtils = Utils()
+
+print(myUtils.list2csv.__doc__)
+
+myUtils.list2csv(ListTest,"report.csv")
